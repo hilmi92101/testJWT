5f5ff49c4f48e7a6832b3b502f04d33143e35630
Hilmi <hilmi.azman@srglobal.com>
2020-11-04 18:43:06 +0800

    Create superadmins table

--- database/migrations/2020_11_04_104001_create_superadmins_table.php
+++ database/migrations/2020_11_04_104001_create_superadmins_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAdminsTable extends Migration
+class CreateSuperadminsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateAdminsTable extends Migration
      */
     public function up()
     {
-        Schema::create('admins', function (Blueprint $table) {
+        Schema::create('superadmins', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
@@ -29,6 +29,6 @@ class CreateAdminsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('admins');
+        Schema::dropIfExists('superadmins');
     }
 }
